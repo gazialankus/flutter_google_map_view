dd4c94e2c7d29fdaea68e52221e6f0de8b5ac3f3
ecampohermoso <ecampohermoso@cirrus-it.net>
2018-11-08 18:02:52 -0400

    Putting placeholder for token

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,7 +11,7 @@ import 'package:cirrus_map_view/polyline.dart';
 /// - Static Maps API
 /// - Android Maps API
 /// - iOS Maps API
-const API_KEY = "AIzaSyARkcsczb9w5PBLpxBHBdbsLBIbIjWlmsM";
+const API_KEY = "GOOGLE_MAP_TOKEN_HERE";
 
 void main() {
   MapView.setApiKey(API_KEY);
